ca4f89a1767cffaa3bfd7af56273fdbdad665b75
Renaira Santos <renairacs@yahoo.com.br>
2016-09-18 23:22:58 -0300

    correção na linha do render
    faltou fechar um }

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $('table').dataTable({
 ajax: './data/estabelecimentos.json',
 dom: '<tip>',
 columns: [
-    { data: 'imagem', render: function (data, type, row) { return '<img src="'+data+'" />' },
+    { data: 'imagem', render: function (data, type, row) { return '<img src="'+data+'" />' } },
     { data: 'estabelecimento' },      
     { data: 'tipo' },
     { data: 'endereco' },
