065fe6e06e9d084b939953fa84b474f2d79eb12d
Renaira Santos <renairacs@yahoo.com.br>
2016-09-18 22:02:13 -0300

    mapeando as colunas

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $('table').dataTable({
-ajax: './data/estabelecimentos.json'
+ajax: './data/estabelecimentos.json',
+columns: [
+    { data: 'imagem' },
+    { data: 'estabelecimento' },      
+    { data: 'tipo' },
+    { data: 'endereco' },
+    { data: 'estado' },
+    { data: 'cidade' } /* Mapeia colunas */
+]
 });
